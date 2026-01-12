f09b68b2def4403f26415339ac267847570bda6f
fl360travel-boop <fl360travel@gmail.com>
2026-01-12 16:03:30 -0300

    Create Sidebar.tsx

---
+++ components/Sidebar.tsx
@@ -0,0 +1,8 @@
+export default function Sidebar() {
+  return (
+    <aside style={{ padding: 16, borderRight: "1px solid #333", minHeight: "100vh" }}>
+      <h2 style={{ margin: 0 }}>Menu</h2>
+      <p style={{ marginTop: 8, opacity: 0.8 }}>Sidebar criada.</p>
+    </aside>
+  );
+}
