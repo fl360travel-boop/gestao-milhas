066e2a24c7b6f224ddcd916765af24dfdd7884c6
fl360travel-boop <fl360travel@gmail.com>
2026-01-12 18:39:19 -0300

    TransferForm.tsx

---
+++ pages/TransferForm.tsx
@@ -0,0 +1,8 @@
+export default function TransferForm() {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1>Transferência de Milhas</h1>
+      <p>Formulário de transferência.</p>
+    </div>
+  );
+}
