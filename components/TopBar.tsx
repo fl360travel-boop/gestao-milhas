e7edd2761cf161509f478ec1eff2c9e3058c1b0e
fl360travel-boop <fl360travel@gmail.com>
2026-01-12 16:19:30 -0300

    Create TopBar component

---
+++ components/TopBar.tsx
@@ -0,0 +1,7 @@
+export default function TopBar() {
+  return (
+    <header style={{ padding: 16, borderBottom: "1px solid #333" }}>
+      <h1 style={{ margin: 0, fontSize: 18 }}>Gestão de Milhas</h1>
+    </header>
+  );
+}
