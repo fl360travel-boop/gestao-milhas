27ba0a643c79a61391214aafed23477ad1683e46
fl360travel-boop <fl360travel@gmail.com>
2026-01-12 18:37:30 -0300

    StrategicSummary.tsx

---
+++ pages/StrategicSummary.tsx
@@ -0,0 +1,8 @@
+export default function StrategicSummary() {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1>Resumo Estratégico</h1>
+      <p>Visão estratégica das milhas.</p>
+    </div>
+  );
+}
