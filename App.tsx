359ff2566c27be2865c0f43f75b497ba13e71bd5
fl360travel-boop <fl360travel@gmail.com>
2026-01-12 17:33:20 -0300

    App.tsx

--- App.tsx
+++ App.tsx
@@ -1,46 +1,14 @@
-import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-
-import Sidebar from './components/Sidebar';
-import TopBar from './components/TopBar';
-
-import Dashboard from './pages/Dashboard';
-import Operations from './pages/Operations';
-import Clients from './pages/Clients';
-import Onboarding from './pages/Onboarding';
-import StrategicSummary from './pages/StrategicSummary';
-import Alerts from './pages/Alerts';
-import TransferForm from './pages/TransferForm';
-import SaleForm from './pages/SaleForm';
-import RedemptionForm from './pages/RedemptionForm';
-import Concierge from './pages/Concierge';
-import Settings from './pages/Settings';
-import Login from './pages/Login';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 export default function App() {
   return (
     <HashRouter>
-      <TopBar />
-      <div style={{ display: 'flex' }}>
-        <Sidebar />
-        <main style={{ padding: 24, flex: 1 }}>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/operations" element={<Operations />} />
-            <Route path="/clients" element={<Clients />} />
-            <Route path="/onboarding" element={<Onboarding />} />
-            <Route path="/strategic-summary" element={<StrategicSummary />} />
-            <Route path="/alerts" element={<Alerts />} />
-            <Route path="/transfer" element={<TransferForm />} />
-            <Route path="/sale" element={<SaleForm />} />
-            <Route path="/redemption" element={<RedemptionForm />} />
-            <Route path="/concierge" element={<Concierge />} />
-            <Route path="/settings" element={<Settings />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </main>
-      </div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </HashRouter>
   );
 }
-
